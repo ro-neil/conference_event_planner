314096c5e599e5f9b4d698449062cbf6f2f4e46e
ro-neil <roneilbarrett@gmail.com>
2025-08-15 12:34:04 -0400

    Added initial state and reducer functions to mealSlice state

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -4,10 +4,17 @@ import { createSlice } from '@reduxjs/toolkit';
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
-   
+    { name: 'Breakfast', cost: 50, selected: false },
+    { name: 'High Tea', cost: 25, selected: false },
+    { name: 'Lunch', cost: 65, selected: false },
+    { name: 'Dinner', cost: 70, selected: false },
   ],
   reducers: {
     toggleMealSelection: (state, action) => {
+        const meal = state[action.payload];
+        if (meal) {
+            meal.selected = !meal.selected;
+        }
     },
   },
 });
